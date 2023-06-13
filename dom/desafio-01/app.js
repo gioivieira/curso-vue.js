0082dc163ba03b0491e3f2392e88b169b2df3156
GIovana Vieira <gioivieira@gmail.com>
2023-06-13 11:17:31 -0300

    V5, V6 e desafio 01.

---
+++ dom/desafio-01/app.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: "#desafio",
+    data: {
+        name: "Giovana",
+        age: 24,
+        image: "https://www.filhao.com.br/noticia/adicionais/90-66-criando-arte-com-inteligencia-artificial-midjourney-e-outras.png"
+    },
+    methods: {
+        randomNumber() {
+            return Math.random()
+        }
+    }
+})
