5aad2097938e2aa996c4f00140bdd4c8135084f6
GIovana Vieira <gioivieira@gmail.com>
2023-07-07 18:13:47 -0300

    Finalização do projeto-01.

--- projeto01-curso-vue.js/app.js
+++ projeto01-curso-vue.js/app.js
@@ -7,9 +7,44 @@ new Vue({
         ataqueJogador: 0,
         listaAtaques: [],
         iniciarClicado: true,
-        vitorioso: null     
+        vitorioso: undefined,
+        widthJogador: 100,
+        widthMonstro: 100,
+        backgroundJogador: 'green',
+        backgroundMonstro: 'green'      
+    },
+    computed: {
+        jogadorPorcentagem() {
+            return {
+                height: '3vh',
+                backgroundColor: this.backgroundJogador,
+                width: `${this.widthJogador}%`
+            }
+        },
+        monstroPorcentagem() {
+            return {
+                height: '3vh',
+                backgroundColor: this.backgroundMonstro,
+                width: `${this.widthMonstro}%`
+            }
+        }
     },
     methods: {
+        lista(i) {
+            if(i % 2 !== 0){
+                return {
+                    backgroundColor: 'lightBlue',
+                    width: '100%',
+                    height: '3vh'
+                }
+            } else {
+                return {
+                    backgroundColor: 'orange',
+                    width: '100%',
+                    height: '3vh'
+                }
+            }
+        },
         voceGanhou() {
             if(this.vitorioso && this.porcentagemJogador !== 0 && this.porcentagemMonstro !== 0) {
                 return true
@@ -29,21 +64,32 @@ new Vue({
             }
         },
         botaoIniciar() {
-            this.iniciarClicado = !this.iniciarClicado
+            this.iniciarClicado = false
             this.vitorioso = null
             this.listaAtaques = []
             this.porcentagemJogador = 100
             this.porcentagemMonstro = 100
+            this.widthJogador = 100
+            this.widthMonstro = 100
+            this.backgroundJogador = 'green' 
+            this.backgroundMonstro = 'green'
         },
         botaoDesistir() {
             this.iniciarClicado = !this.iniciarClicado
             this.porcentagemJogador = 100 
-            this.porcentagemMonstro = 100 
+            this.porcentagemMonstro = 100
+            this.widthJogador = 100
+            this.widthMonstro = 100
+            this.backgroundJogador = 'green' 
+            this.backgroundMonstro = 'green'
         },
         funcaoAtaque() {
             if(this.porcentagemMonstro !== 0 && this.porcentagemJogador !== 0){
                 this.ataqueMonstro = Math.floor(Math.random() * 10 + 10)
-                this.ataqueJogador = this.ataqueMonstro - Math.floor(Math.random() * 10 + 1)    
+                this.ataqueJogador = this.ataqueMonstro - Math.floor(Math.random() * 10 + 1)
+
+                this.widthJogador -= this.ataqueMonstro
+                this.widthMonstro -= this.ataqueJogador 
 
                 this.porcentagemJogador -= this.ataqueMonstro
                 this.porcentagemMonstro -= this.ataqueJogador 
@@ -52,10 +98,18 @@ new Vue({
                 this.listaAtaques.push(`Jogador atingiu monstro com ${this.ataqueJogador}.`) 
             }
 
+            if(this.porcentagemJogador < 20){
+                this.backgroundJogador = 'red'
+            } if(this.porcentagemMonstro < 20){
+                this.backgroundMonstro = 'red'
+            }
+
             if(this.porcentagemJogador < 0){
                 this.porcentagemJogador = 0
+                this.widthJogador = 0
             }if(this.porcentagemMonstro < 0){
                 this.porcentagemMonstro = 0
+                this.widthMonstro = 0
             }if(this.porcentagemJogador === 0){
                 this.vitorioso = false
             } if(this.porcentagemMonstro === 0){
@@ -65,7 +119,10 @@ new Vue({
         funcaoAtaqueEspecial() {
             if(this.porcentagemMonstro !== 0 && this.porcentagemJogador !== 0){
                 this.ataqueJogador = Math.floor(Math.random() * 10 + 10)
-                this.ataqueMonstro = this.ataqueJogador - Math.floor(Math.random() * 10 + 1)    
+                this.ataqueMonstro = this.ataqueJogador - Math.floor(Math.random() * 10 + 1) 
+                
+                this.widthJogador -= this.ataqueMonstro
+                this.widthMonstro -= this.ataqueJogador 
 
                 this.porcentagemJogador -= this.ataqueMonstro
                 this.porcentagemMonstro -= this.ataqueJogador 
@@ -74,10 +131,18 @@ new Vue({
                 this.listaAtaques.push(`Jogador atingiu monstro com ${this.ataqueJogador}.`)
             }
 
+            if(this.porcentagemJogador < 20){
+                this.backgroundJogador = 'red'
+            } if(this.porcentagemMonstro < 20){
+                this.backgroundMonstro = 'red'
+            }
+
             if(this.porcentagemMonstro < 0){
                 this.porcentagemMonstro = 0
+                this.widthMonstro = 0
             }if(this.porcentagemJogador < 0){
                 this.porcentagemJogador = 0
+                this.widthJogador = 0
             }if(this.porcentagemMonstro === 0){
                 this.vitorioso = true
             } if(this.porcentagemJogador === 0){
@@ -88,25 +153,39 @@ new Vue({
         funcaoCurar() {
             if(this.porcentagemMonstro !== 0 && this.porcentagemJogador !== 0){
                 this.ataqueMonstro = Math.floor(Math.random() * 10 + 10)
-                this.defesaJogador = Math.floor(Math.random() * 10 + 10)    
+                this.defesaJogador = Math.floor(Math.random() * 10 + 10) 
 
                 if(this.porcentagemJogador !== 100 && this.porcentagemJogador < 100){
                 this.porcentagemJogador = (this.porcentagemJogador + this.defesaJogador) - this.ataqueMonstro
-    
+                this.widthJogador = (this.porcentagemJogador + this.defesaJogador) - this.ataqueMonstro
+
+                if(this.porcentagemJogador >= 100){
+                    this.widthJogador = 100
+                    this.porcentagemJogador = 100
+                }
+
                 this.listaAtaques.push(`Monstro atingiu jogador com ${this.ataqueMonstro}.`)
                 this.listaAtaques.push(`Jogador ganhou força de ${this.ataqueJogador}.`)
                 }
             }
 
-            if(this.porcentagemJogador < 0){
-                this.porcentagemJogador = 0
-            }if(this.porcentagemMonstro < 0){
+            if(this.porcentagemJogador < 20){
+                this.backgroundJogador = 'red'
+            } if(this.porcentagemJogador >= 20){
+                this.backgroundJogador = 'green'
+            } 
+
+            if(this.porcentagemMonstro < 0){
                 this.porcentagemMonstro = 0
-            }if(this.porcentagemJogador === 0){
-                this.vitorioso = false
-            } if(this.porcentagemMonstro === 0){
+                this.widthMonstro = 0
+            }if(this.porcentagemJogador < 0){
+                this.porcentagemJogador = 0
+                this.widthJogador = 0
+            }if(this.porcentagemMonstro === 0){
                 this.vitorioso = true
-            }
+            } if(this.porcentagemJogador === 0){
+                this.vitorioso = false
+            } 
         }
     }
 })
