5dc0caed418256cdd51eddea0352c6e13e9d1bcd
Giovana Vieira <gioivieira@gmail.com>
2023-07-20 20:34:10 -0300

    vue-cli

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">{{ titulo }}
+    <button @click="titulo += '#'">Alterar</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        titulo: 'Usando Vue.js!'
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    background-color: brown;
+    color: white;
+  }
+</style>
